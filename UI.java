ae62fb9604cd7f6f4cdef42a500bce1892e38b62
larzyArt <laurencebaraga38@gmail.com>
2025-10-16 18:05:46 +0800

    added the chapters

--- UI.java
+++ UI.java
@@ -15,20 +15,27 @@ public class UI {
     }
 
     public void displayMenu(){
-        System.out.println("==== Main Menu ====\n");
+        System.out.println("==== *Princess Academy: Worlds Collide* ====\n");
         System.out.println("What do you want to do?");
-        System.out.println("1. Start Game");
-        System.out.println("2. Show Stats");
+        System.out.println("1. Play Story Mode");
+        System.out.println("2. Character/Mob Stats");
         System.out.println("3. Credits");
         System.out.println("4. Exit");
-        System.out.println("====================\n");
+        System.out.println();
+        System.out.println("====================");
         System.out.print("Enter next Action: ");
     }
 
     public void prompt(int choice){
-        if(choice == 1){ // Attacks
-            System.out.println("\nAttack function is non existent");
-            System.out.println("Returning to main menu...");
+        if(choice == 1){ // Starts Game
+                StartGame();
+                // Wait for the user to press Enter so the menu doesn't immediately reappear
+
+                try {
+                    skillInput.nextLine();
+                } catch (Exception e) {
+
+                }
             }else if(choice == 2){ // Shows Stats
                 System.out.println();
                 showStats();
@@ -118,6 +125,78 @@ public class UI {
                 System.out.println("Returning to main menu...\n");
 
             }
+        }
+    }
+
+    public void StartGame(){
+        Dialogue dialogue = new Dialogue();
+        int chapter = -1;
+        System.out.println("==== Story Mode ====\n");
+        System.out.println("Chapter 1: Battle of the Sea");
+        System.out.println("Chapter 2: The Survival");
+        System.out.println("Chapter 3: Darkness and Void");
+        System.out.println("Chapter 4: Heart of the Living Forest");
+        System.out.println("Chapter 5: The Castle Memoir");
+        System.out.println("Chapter 6: The Final Show");
+        System.out.println("====================");
+        System.out.println("Enter Chapter Number to play (1-6), or 0 to return to the main menu.");
+        System.out.print("Enter Action: ");
+
+        try {
+            chapter = skillInput.nextInt();
+        } catch (Exception e) {
+            System.out.println();
+            // invalid input, consume the token and return
+            skillInput.next();
+            System.out.println("Invalid input. Returning to main menu...");
+            return;
+        }
+
+        if (chapter == 0) {
+            System.out.println("Returning to main menu...");
+            return; // return to menu
+        }
+
+        if (chapter >= 1 && chapter <= 6) {
+            System.out.println("Starting Chapter " + chapter + "...");
+        } else {
+            System.out.println("Invalid chapter. Returning to main menu...");
+        }
+        switch(chapter){
+            case 1:
+                dialogue.sirenEmpressInteraction();
+                dialogue.sirenEmpressDefeated();
+                break;
+            case 2:
+                dialogue.lavaBeastInteraction();
+                dialogue.lavaBeastDefeated();
+                break;
+
+            case 3:
+                dialogue.eclipseCoreInteraction();
+                dialogue.eclipseCoreDefeated(); 
+                break;
+            case 4:
+                dialogue.resonaraInteraction();
+                dialogue.resonaraDefeated();
+                break;
+            case 5:
+                dialogue.kassundreInteraction();
+                dialogue.kassundreDefeated();
+                break;
+            case 6:
+                dialogue.twinkleInteraction();
+                dialogue.twinkleDefeated();
+                break;
+        }
+
+        try{
+            System.out.println("Press Enter to return to main menu...");
+            skillInput.nextLine();
+        }
+        catch(Exception e){
+            System.out.println();
+            System.out.println("Returning to main menu...");
         }
     }
 }
